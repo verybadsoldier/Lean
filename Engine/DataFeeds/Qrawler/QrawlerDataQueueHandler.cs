2eefd13ff0129a34be725b56e8778e47cb437bce
verybadsoldier <vbs@springrts.de>
2019-12-12 22:51:58 +0100

    improved qrawler integration

--- Engine/DataFeeds/Qrawler/QrawlerDataQueueHandler.cs
+++ Engine/DataFeeds/Qrawler/QrawlerDataQueueHandler.cs
@@ -4,39 +4,47 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using Qrawler.Api;
 using Qrawler.Interfaces.WebSocket;
+using QuantConnect.Configuration;
 using QuantConnect.Data;
 using QuantConnect.Data.Market;
 using QuantConnect.Interfaces;
 using QuantConnect.Packets;
-using QuantConnect.Util;
 using QSymbol = QrawlerEngine.Models.Symbol;
 
 namespace QuantConnect.Lean.Engine.DataFeeds.Qrawler
 {
     class QrawlerDataQueueHandler : IDataQueueHandler
     {
-        private LiveData _qlive;
+        private readonly LiveData _qlive;
         private readonly HashSet<Symbol> _symbols = new HashSet<Symbol>();
 
         public QrawlerDataQueueHandler()
         {
-            var wsfactory = new WebSocket4NetFactory();
-            _qlive = new LiveData(wsfactory,"ws://127.0.0.1:7772/live");
+            _qlive = new LiveData(new WebSocket4NetFactory(), Config.GetValue<string>("qrawler.uri"));
+            _qlive.Start();
         }
+
         public IEnumerable<BaseData> GetNextTicks()
         {
-            foreach (QrawlerEngine.Models.Tick VARIABLE in _qlive.GetTicks())
+            foreach (QrawlerEngine.Models.Tick tickIn in _qlive.GetTicks())
             {
-                Symbol s = _symbols.First(x => x.Value == VARIABLE.Symbol);
-                yield return new Tick(VARIABLE.Time.ToDateTimeUtc(), s, VARIABLE.Bid, VARIABLE.Ask);
+                Symbol s = _symbols.First(x => x.Value == tickIn.Symbol);
+                yield return new Tick
+                {
+                    Time = tickIn.Time.ToDateTimeUtc(),
+                    Symbol = s,
+                    Value = tickIn.Last,
+                    TickType = TickType.Trade,
+                    Quantity = tickIn.Volume.GetValueOrDefault(0),
+                };
             }
         }
 
         public void Subscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
         {
-            _qlive.SubscribeSymbols(symbols.Select(x => new QSymbol(x.Value)).ToList());
+            _qlive.SubscribeSymbols(symbols.Select(x => new QSymbol(x.Value, "*", "GodmodeTrader")).ToList());
 
-            foreach (var s in symbols)
+            foreach (Symbol s in symbols)
             {
                 _symbols.Add(s);
             }
